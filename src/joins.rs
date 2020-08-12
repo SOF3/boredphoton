3e14ec26be1a01247ca9ef56e3f43e53cdba49bd
SOFe <sofe2038@gmail.com>
2020-08-12 11:15:37 +0800

    Rewrote joins algorithm

---
+++ src/joins.rs
@@ -0,0 +1,207 @@
+use std::collections::{HashMap, VecDeque};
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+use std::sync::Mutex;
+
+use std::time::UNIX_EPOCH;
+
+use crossbeam::sync::ShardedLock;
+use serde::{Deserialize, Serialize};
+use serenity::model::id;
+
+const BACKLOG_SIZE: usize = 720;
+
+pub struct GuildJoinsMap {
+    lock: ShardedLock<HashMap<id::GuildId, Mutex<GuildJoins>>>,
+    data_dir: PathBuf,
+}
+
+impl GuildJoinsMap {
+    pub fn new(data_dir: PathBuf) -> Self {
+        Self {
+            lock: ShardedLock::default(),
+            data_dir,
+        }
+    }
+
+    pub fn save(&self) -> io::Result<()> {
+        let write = self.lock.write().unwrap();
+        for gj in write.values() {
+            gj.lock().unwrap().save()?;
+        }
+        Ok(())
+    }
+
+    fn run<F, R>(&self, guild: id::GuildId, f: F) -> R
+    where
+        F: FnOnce(&mut GuildJoins) -> R,
+    {
+        {
+            let read = self.lock.read().unwrap();
+            if let Some(gj) = read.get(&guild) {
+                let mut lock = gj.lock().unwrap();
+                return f(&mut lock);
+            }
+        }
+
+        {
+            let path = self.data_dir.join(&format!("{}.json", guild));
+            let mut write = self.lock.write().unwrap();
+            let gj = write
+                .entry(guild)
+                .or_insert_with(|| Mutex::new(GuildJoins::read_or_new(path)));
+            let mut lock = gj.lock().unwrap();
+            f(&mut lock)
+        }
+    }
+
+    pub fn add(&self, guild: id::GuildId, delta: u32) -> io::Result<Stat> {
+        self.run(guild, move |gj| {
+            gj.add(delta)?;
+            gj.stat()
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GuildJoins {
+    current_hour: u64,
+    log: VecDeque<Option<u32>>,
+    current: u32,
+    #[serde(skip)]
+    path: PathBuf,
+}
+
+impl GuildJoins {
+    pub fn read_or_new(path: PathBuf) -> Self {
+        Self::read(path.clone()).unwrap_or_else(|_| Self::new(path))
+    }
+
+    pub fn new(path: PathBuf) -> Self {
+        Self {
+            current_hour: current_hour(),
+            log: std::iter::repeat(None).take(BACKLOG_SIZE).collect(),
+            current: 0,
+            path,
+        }
+    }
+
+    pub fn read(path: PathBuf) -> Result<Self, std::io::Error> {
+        let f = fs::File::open(&path)?;
+        let mut de: Self =
+            serde_json::from_reader(f).map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+        de.path = path;
+        de.update_to_latest_hour(true)?;
+        Ok(de)
+    }
+
+    pub fn save(&self) -> io::Result<()> {
+        let f = fs::File::create(&self.path)?;
+        serde_json::to_writer(f, self).map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+        Ok(())
+    }
+
+    pub fn update_to_latest_hour(&mut self, fill_with_none: bool) -> io::Result<()> {
+        let now = current_hour();
+        assert!(self.current_hour <= now, "System clock travelled backwards");
+
+        let need_save = self.current_hour != now;
+
+        if self.current_hour < now {
+            self.current_hour += 1;
+            self.log.pop_front();
+            self.log.push_back(Some(self.current));
+            self.current = 0;
+        }
+
+        // could make this O(1), but no need for that complexity
+        while self.current_hour < now {
+            self.current_hour += 1;
+            self.log.pop_front();
+            self.log.push_back(match fill_with_none {
+                true => None,
+                false => Some(0),
+            });
+            self.current = 0;
+        }
+
+        if need_save {
+            self.save()?;
+        }
+
+        Ok(())
+    }
+
+    pub fn add(&mut self, delta: u32) -> io::Result<()> {
+        self.update_to_latest_hour(false)?;
+        self.current += delta;
+        Ok(())
+    }
+
+    pub fn stat(&mut self) -> io::Result<Stat> {
+        self.update_to_latest_hour(false)?;
+
+        let mut data: Vec<_> = self
+            .log
+            .iter()
+            .copied()
+            .filter_map(|option| option)
+            .map(|int| int as f64)
+            .collect();
+        // we can't have NANs from (int as f64)
+        data.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        Ok(Stat {
+            mean: data.iter().copied().sum::<f64>() / (data.len() as f64),
+            max: get_percentile(&data, 1.),
+            uq: get_percentile(&data, 0.75),
+            median: get_percentile(&data, 0.5),
+            lq: get_percentile(&data, 0.25),
+            min: get_percentile(&data, 0.),
+            n: data.len(),
+            current: self.current,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Stat {
+    mean: f64,
+    max: f64,
+    uq: f64,
+    median: f64,
+    lq: f64,
+    min: f64,
+    n: usize,
+    current: u32,
+}
+
+impl Stat {
+    pub fn is_abnormal(&self) -> bool {
+        (self.current as f64) > self.uq * 5.
+    }
+}
+
+fn current_hour() -> u64 {
+    UNIX_EPOCH
+        .elapsed()
+        .expect("System clock is earlire than unix epoch")
+        .as_secs()
+        / 3600
+}
+
+pub fn get_percentile(slice: &[f64], ratio: f64) -> f64 {
+    let position = linterp(0., slice.len() as f64, ratio);
+    let low = position.trunc() as usize;
+    let high = low + 1;
+    if high >= slice.len() {
+        slice[low]
+    } else {
+        linterp(slice[low], slice[high], position.fract())
+    }
+}
+
+pub fn linterp(l: f64, r: f64, k: f64) -> f64 {
+    l * (1. - k) + r * k
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::fmt;
 use std::fs;
 use std::io;
-use std::path::{Path, PathBuf};
-use std::sync::atomic::{AtomicU32, Ordering};
-use std::sync::RwLock;
-use std::time::UNIX_EPOCH;
+use std::path::Path;
 
-use crossbeam::sync::ShardedLock;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use serenity::client::Context;
 use serenity::model::*;
+
+mod joins;
+use joins::*;
 
 type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
@@ -111,27 +109,23 @@ impl serenity::client::EventHandler for Handler {
                     "invite" => {
                         message.reply(&ctx, format!("Invite link: {}", &self.invite_link))?;
                     }
+                    "stat" => {
+                        if let Some(guild) = message.guild_id {
+                            let stat = self.guild_joins.add(guild, 0)?;
+                            message.reply(&ctx, format!("Stats: {:?}", stat))?;
+                        }
+                    }
                     "adm" => {
                         if !ADMINS.contains(message.author.id.as_u64()) {
                             return Ok(());
                         }
                         match args.next() {
                             Some("save") => {
-                                self.guild_joins.save();
+                                self.guild_joins.save()?;
                             }
                             Some("stop") => {
-                                self.guild_joins.save();
+                                self.guild_joins.save()?;
                                 std::process::exit(0);
-                            }
-                            Some("stat") => {
-                                if let Some(guild) = message.guild_id {
-                                    let AddResult { current, stats } =
-                                        self.guild_joins.query(guild);
-                                    message.reply(
-                                        &ctx,
-                                        format!("Current = {}\nStats: {}", current, stats,),
-                                    )?;
-                                }
                             }
                             _ => (),
                         }
@@ -148,22 +142,17 @@ impl serenity::client::EventHandler for Handler {
         trying(|| {
             let guild = guild::Guild::get(&ctx, guild_id)?;
 
-            let AddResult { current, stats } = self.guild_joins.add(guild_id, 1);
+            let stat = self.guild_joins.add(guild_id, 1)?;
 
-            log::info!(
-                "Guild {} current = {}\nStats: {}",
-                &guild.name,
-                current,
-                stats,
-            );
+            log::info!("Guild {} stats: {:?}", &guild.name, &stat,);
 
-            if stats.is_abnormal(current) {
+            if stat.is_abnormal() {
                 if let Some(&channel) = self.channels.get(&guild_id.as_u64()) {
                     let channel = id::ChannelId::from(channel);
                     channel.send_message(&ctx, |m| {
                         m.content(format!(
-                            "@here ALERT: abnormal server joins detected, current = {}, stats = {}",
-                            current, &stats
+                            "@here ALERT: abnormal server joins detected, stats = {:?}",
+                            &stat
                         ))
                     })?;
                 }
@@ -171,199 +160,6 @@ impl serenity::client::EventHandler for Handler {
 
             Ok(())
         })
-    }
-}
-
-const JOINS_BACKLOG_SIZE: usize = 720;
-
-fn current_hour() -> u64 {
-    UNIX_EPOCH
-        .elapsed()
-        .expect("System clock is earlire than unix epoch")
-        .as_secs()
-        / 3600
-}
-
-struct GuildJoinsMap {
-    lock: ShardedLock<HashMap<id::GuildId, RwLock<GuildJoins>>>,
-    data_dir: PathBuf,
-}
-
-impl GuildJoinsMap {
-    pub fn new(data_dir: PathBuf) -> Self {
-        Self {
-            lock: ShardedLock::default(),
-            data_dir,
-        }
-    }
-
-    pub fn save(&self) {
-        let lock = self.lock.write().unwrap();
-
-        for (id, gj) in lock.iter() {
-            let gj = gj.write().unwrap();
-            if let Err(err) = gj.save(self.data_dir.join(format!("{}.json", id))) {
-                log::error!("Error saving guild joins: {}", err);
-            }
-        }
-    }
-
-    pub fn add(&self, guild: id::GuildId, count: u32) -> AddResult {
-        loop {
-            {
-                let guard = self.lock.read().unwrap();
-                if let Some(gj) = guard.get(&guild) {
-                    {
-                        let gjg = gj.read().unwrap();
-                        if let Ok(current) = gjg.add(count) {
-                            let stats = gjg.stats();
-                            return AddResult { current, stats };
-                        }
-                    }
-
-                    {
-                        let mut gjg = gj.write().unwrap();
-                        let current = gjg.add_mut(count);
-                        let stats = gjg.stats();
-                        return AddResult { current, stats };
-                    }
-                }
-            }
-
-            {
-                let mut guard = self.lock.write().unwrap();
-                let gj = GuildJoins::new(self.data_dir.join(format!("{}.json", guild)));
-                let _ = guard.insert(guild, RwLock::new(gj));
-            }
-        }
-    }
-
-    pub fn query(&self, guild: id::GuildId) -> AddResult {
-        self.add(guild, 0)
-    }
-}
-
-struct AddResult {
-    current: u32,
-    stats: Stats,
-}
-
-#[derive(Serialize, Deserialize)]
-struct GuildJoins {
-    current_hour: u64,
-    log: VecDeque<Option<u32>>,
-    current: AtomicU32,
-}
-
-impl GuildJoins {
-    fn new(path: impl AsRef<Path>) -> Self {
-        Self::load(path).unwrap_or_else(|| Self {
-            current_hour: current_hour(),
-            log: std::iter::repeat(None).take(JOINS_BACKLOG_SIZE).collect(),
-            current: AtomicU32::new(0),
-        })
-    }
-
-    fn load(path: impl AsRef<Path>) -> Option<Self> {
-        serde_json::from_reader(fs::File::open(path).ok()?).ok()
-    }
-
-    fn save(&self, path: impl AsRef<Path>) -> Result<()> {
-        serde_json::to_writer(fs::File::create(path)?, self)?;
-        Ok(())
-    }
-
-    fn add(&self, count: u32) -> Result<u32, ()> {
-        let hour = current_hour();
-        if hour > self.current_hour {
-            return Err(());
-        }
-        Ok(self.current.fetch_add(count, Ordering::Relaxed))
-    }
-
-    fn add_mut(&mut self, count: u32) -> u32 {
-        let hour = current_hour();
-        let current = self.current.get_mut();
-
-        if hour > self.current_hour {
-            let diff = (hour - self.current_hour) as usize;
-            if diff >= JOINS_BACKLOG_SIZE {
-                // reinitialize to all Nones
-                self.log = std::iter::repeat(None).take(JOINS_BACKLOG_SIZE).collect();
-            } else {
-                drop(self.log.drain(..diff));
-                self.log.extend(std::iter::repeat(None).take(diff));
-            }
-            let back = self.log.back_mut().expect("JOINS_BACKLOG_SIZE > 0");
-            *back = Some(*current);
-            *current = 0;
-        }
-
-        debug_assert_eq!(self.log.len(), JOINS_BACKLOG_SIZE);
-
-        *current += count;
-        *current
-    }
-
-    fn stats(&self) -> Stats {
-        dbg!(&self.log);
-
-        let mut sorted: Vec<u32> = self.log.iter().filter_map(|option| *option).collect();
-        sorted.sort();
-
-        if sorted.is_empty() {
-            return Stats::Empty;
-        }
-
-        let count = sorted.len() as f64;
-        let sum = sorted.iter().sum::<u32>() as f64;
-
-        fn weighted_ind(sorted: &[u32], ind: f64) -> f64 {
-            let trunc = ind.trunc();
-            if trunc < 0. {
-                return sorted[0] as f64;
-            }
-            if trunc > sorted.len() as f64 - 1. {
-                return *sorted.last().unwrap() as f64;
-            }
-            let frac = ind.fract();
-            let floor = sorted[ind as usize];
-            let ceil = sorted[ind as usize + 1];
-            (floor as f64) * frac + (ceil as f64) * (1. - frac)
-        }
-
-        Stats::Data {
-            average: sum / count,
-            lq: weighted_ind(&sorted[..], (count - 1.) * 0.25),
-            median: weighted_ind(&sorted[..], (count - 1.) * 0.5),
-            uq: weighted_ind(&sorted[..], (count - 1.) * 0.75),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum Stats {
-    Empty,
-    Data {
-        average: f64,
-        lq: f64,
-        median: f64,
-        uq: f64,
-    },
-}
-
-impl Stats {
-    fn is_abnormal(&self, current: u32) -> bool {
-        match self {
-            Self::Empty => false,
-            Self::Data { uq, .. } => (current as f64) > uq * 5.,
-        }
-    }
-}
-
-impl fmt::Display for Stats {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
     }
 }
 
